0e1dcfa8db01c8ea4b9825b4ac43cd38c41b6d53
ArxFelxis <176923291+ArxFelxis@users.noreply.github.com>
2026-01-16 12:28:21 +0800

    Project Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,119 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialDetails = {
+  name: 'Arx Felxis',
+  email: 'arxfelx@gmail.com',
+  phoneNumber: '00000000',
+  university: 'University of Singapore',
+  degree: 'Bachelor of Science in Computer Engineering',
+  year: '2020 - 2024',
+  company: 'Web Dev Co.',
+  workingPeriod: '2024 - Present',
+  jobDescription: 'Assisting in Front End Development'
+}
+
+export default function App () {
+  const [details, setDetails] = useState(initialDetails)
+  const [isEditing, setIsEditing] = useState(false)
+
+  const handleChangeInput = e => {
+    const {name, value} = e.target
+    setDetails({
+      ...details,
+      [name]: value
+  })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {
+        isEditing ?
+        <EditForm data={details} onSave={() => setIsEditing(false)} onChange={handleChangeInput} /> :
+        <DisplayDetails data={details} onEdit={() => setIsEditing(true)} />
+      }
+    </div>
   )
 }
 
-export default App
+function EditForm ({data, onSave, onChange}) {
+  return (
+    <div id='form'>
+      <h3 id='formHeader'>Edit Details</h3>
+      <input
+        name='name'
+        placeholder='Name'
+        value={data.name}
+        onChange={onChange} />
+      <input
+        name='email'
+        placeholder='Email'
+        value={data.email}
+        onChange={onChange} />
+      <input
+      name='phoneNumber'
+      placeholder='Phone Number'
+      value={data.phoneNumber}
+      onChange={onChange} />
+      <input
+        name='university'
+        placeholder='University'
+        value={data.university}
+        onChange={onChange} />
+      <input
+        name='degree'
+        placeholder='Degree'
+        value={data.degree}
+        onChange={onChange} />
+      <input
+        name='year'
+        placeholder='Years of Study'
+        value={data.year}
+        onChange={onChange} />
+      <input
+        name='company'
+        placeholder='Company Name'
+        value={data.company}
+        onChange={onChange} />
+      <input
+        name='workingPeriod'
+        placeholder='Working Period'
+        value={data.workingPeriod}
+        onChange={onChange} />
+      <input
+        name='jobDescription'
+        placeholder='Job Description'
+        value={data.jobDescription}
+        onChange={onChange} />
+      <button onClick={onSave}>Save</button>
+    </div>
+  )
+}
+
+function DisplayDetails ({data, onEdit}) {
+
+  return (
+    <div id='resume'>
+      <div id='personalDetails'>
+        <div id='personDetailsLeft'>
+          <h2>{data.name}</h2>
+          <p>{data.email}</p>
+        </div>
+        <p id='phoneNumber'>{data.phoneNumber}</p>
+      </div>
+      <div id='education'>
+        <h1>Education</h1>
+        <p>{data.university}</p>
+        <p>{data.degree}</p>
+        <p>{data.year}</p>
+      </div>
+      <div id='experience'>
+        <h1>Experience</h1>
+        <p>{data.company}</p>
+        <p>{data.workingPeriod}</p>
+        <p>{data.jobDescription}</p>
+      </div>
+      <button id='editBtn' onClick={onEdit}>Edit</button>
+    </div>
+  )
+}
